224e79958901adf51e0b089059c65e0a320a20f2
maklad42 <maklad42+git@gmail.com>
2021-01-29 22:56:03 +0900

    Initial commit. Created the Display, Keypad, and Button components. 210129

---
+++ src/components/Display.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import "./Display.css";
+
+export default class Display extends Component {
+  render() {
+    return (
+      <div className="Display">
+        {this.props.data}
+      </div>
+    );
+  }
+}
+
+export default Display:
---
+++ src/components/Keypad.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+import './Keypad.css';
+
+export default class Keypad extends Component {
+  render() {
+    return <div className="Keypad">{this.props.children}</div>;
+  }
+}
